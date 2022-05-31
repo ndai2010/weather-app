69d2d9beb8c0585c2efbc612f7c0860204fc0a06
Đặng Đại <dangdai2010@gmail.com>
2022-05-31 16:57:39 +0700

    fe

--- src/page/MainPage/MainPage.js
+++ src/page/MainPage/MainPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './MainPage.scss'
 import { connect } from 'react-redux'
-import { Search, Thermostat } from '@mui/icons-material'
+import { Search, Thermostat, Air, Opacity, Explore, LocalDrink } from '@mui/icons-material'
 import * as action from '../../redux/action'
 class MainPage extends Component {
     constructor(props) {
@@ -11,7 +11,12 @@ class MainPage extends Component {
             nameLocation: '',
             weatherIcon: '',
             temp: '',
-            country: ''
+            country: '',
+
+            wind: '',
+            humidity: '',
+            Precipitation: '',
+            Wind_direction: ''
         }
     }
     onChangeInput = (event, id) => {
@@ -42,7 +47,12 @@ class MainPage extends Component {
                 nameLocation: data.location.name,
                 weatherIcon: data.current.condition.icon,
                 temp: data.current.temp_c,
-                country: data.location.country
+                country: data.location.country,
+
+                wind: data.current.wind_kph,
+                humidity: data.current.humidity,
+                Precipitation: data.current.precip_mm,
+                Wind_direction: data.current.wind_dir
             })
         }
     }
@@ -52,7 +62,11 @@ class MainPage extends Component {
         }
     }
     render() {
-        let { weatherIcon, nameLocation, temp, country } = this.state
+        let { weatherIcon, nameLocation, temp, country,
+            wind,
+            humidity,
+            Precipitation,
+            Wind_direction } = this.state
         return (
             <div className='main-page'>
                 <div className='content container'>
@@ -63,8 +77,8 @@ class MainPage extends Component {
                     <div className='weather-title container'>
                         <div className='temp-image'>
                             <div className='temp'>
-                                <Thermostat className='icon' />
-                                <h3>{temp}</h3>
+
+                                <h3><Thermostat className='icon' />{temp}°C</h3>
                             </div>
                             <div className='image-box'>
                                 <img className='image' src={weatherIcon} alt=''></img>
@@ -75,10 +89,36 @@ class MainPage extends Component {
                     <div className='weather-info container'>
                         <span className='title'>Weather info</span>
                         <div className='info-content'>
-                            <div className='item'>1</div>
-                            <div className='item'>2</div>
-                            <div className='item'>3</div>
-                            <div className='item'>4</div>
+                            <div className='item'>
+                                <Air className='icon' />
+                                <div className='title-data'>
+                                    <h5>{wind} km/h</h5>
+                                    <span>Wind speed</span>
+                                </div>
+
+                            </div>
+                            <div className='item'>
+                                <Opacity className='icon' />
+                                <div className='title-data'>
+                                    <h5>{humidity} %</h5>
+                                    <span>Humidity</span>
+                                </div>
+
+                            </div>
+                            <div className='item'>
+                                <LocalDrink className='icon' />
+                                <div className='title-data'>
+                                    <h5>{Precipitation} mm</h5>
+                                    <span>Humidity</span>
+                                </div>
+                            </div>
+                            <div className='item'>
+                                <Explore className='icon' />
+                                <div className='title-data'>
+                                    <h5>{Wind_direction}</h5>
+                                    <span>Wind direction</span>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
